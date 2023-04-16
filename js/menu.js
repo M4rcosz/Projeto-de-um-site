98e53d3ce06030f182996e785a44364eeb5a7b89
M4rcosz <ijohnfelipe71@gmail.com>
2023-04-16 20:46:08 -0300

    grandes mudanças e adições no menu

--- js/menu.js
+++ js/menu.js
@@ -19,4 +19,83 @@ function verificadorCriarConta() {
         confirmationPasswordMessage.style.color = 'Red';
     }
 }
+// Propriedade de elementos - Start
+let loginChoice = document.getElementById("loginForm");
+let creatAccChoice = document.getElementById("creatAccForm");
+let initialCard = document.getElementById("initialCardInfo");
+let loginChoiceButton = document.getElementById("choiceButtonLogin");
+let creatAccChoiceButton = document.getElementById("choiceButtonsLoginAndCreatAccId");
+loginChoice.style.display = "none";
+creatAccChoice.style.display = "none";
+// End
 
+function mouseHoverColorChange(x) {
+    x.addEventListener("mouseover", backColorChoiceButtonChangeMouseover, false);
+    x.addEventListener("mouseout", backColorChoiceButtonChangeMouseout, false);
+
+    function backColorChoiceButtonChangeMouseover() {
+        x.style.backgroundColor = "rgb(33,33,33)"
+    }
+    function backColorChoiceButtonChangeMouseout() {
+        x.style.backgroundColor = "black"
+    }
+}
+function loginBackColorChange() {
+    x = loginChoiceButton;
+    mouseHoverColorChange(x)
+}
+function creatAccBackColorChange() {
+    x = creatAccChoiceButton;
+    mouseHoverColorChange(x)
+}
+loginBackColorChange()
+creatAccBackColorChange()
+// Funções de display - Start
+function initialCardFunction() {
+    if (loginChoice.style.display === "block" || creatAccChoice.style.display === "block") {
+        initialCard.style.display = "none";
+    }
+    else {
+        initialCard.style.display = "block";
+    }
+}
+function choiceButtonLogin() {
+    x = loginChoice;
+    y = loginChoiceButton;
+    z = creatAccChoiceButton;
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+        creatAccChoice.style.display = "none"
+    }
+
+    else {
+        x.style.display = 'none'
+    }
+    initialCardFunction()
+    backColorChoiceButton()
+}
+function choiceButtonCreatAcc() {
+    x = creatAccChoice;
+    y = creatAccChoiceButton;
+    z = loginChoiceButton;
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        loginChoice.style.display = "none"
+    }
+    else {
+        x.style.display = "none"
+    }
+    initialCardFunction()
+    backColorChoiceButton()
+
+}
+//End
+function backColorChoiceButton() {
+    if (x.style.display === 'none') {
+        y.style.backgroundColor = "black"
+    }
+    else if (x.style.display = "block") {
+        y.style.backgroundColor = "rgb(33,33,33)"
+        z.style.backgroundColor = "black"
+    }
+}
